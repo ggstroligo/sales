5fa66b2d7f97edf5b55e038a1a09df4d4a59446a
ggstroligo <gutostroligo@gmail.com>
2021-06-26 20:53:10 -0300

    ( + ) Frontend: Upload report form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class
+    {
+      'notice' => 'alert alert-info',
+      'success' => 'alert alert-success',
+      'error' => 'alert alert-danger',
+      'alert' => 'alert alert-danger'
+    }
+  end
 end
