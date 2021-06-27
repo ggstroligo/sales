9a5cd74ec5d19b753bc7c43b8bdbe87db0726575
ggstroligo <gutostroligo@gmail.com>
2021-06-27 17:58:58 -0300

    ( + ) Frontend improve flash message

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,2 +1,25 @@
 class SalesController < ApplicationController
+  include SalesHelper
+
+  def index
+    @sales = Sale.all
+  end
+
+  def new
+
+  end
+
+  def create
+    SalesReport::Collect
+      .call(raw: sales_file.read)
+      .on_success { |result| flash[:success] = [success_msg(result.data[:payload].size)] }
+      .on_failure { |result| flash[:alert] = result.data[:errors].map { |error| error_msg(error) } }
+      .then { redirect_to sales_path }
+  end
+
+  private
+
+  def sales_file
+    params.require(:report_file).open
+  end
 end
--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,2 +1,18 @@
 module SalesHelper
+  def success_msg(amount_created)
+    {
+      title: 'Sucesso!',
+      description: "Venda com #{amount_created} pedidos cadastrada!"
+    }
+  end
+
+  def error_msg(hash_error)
+    {
+      title: "Linha 1, Coluna #{hash_error[:index]}",
+      description: <<-TEXT
+                  Esperava: #{hash_error[:expected]}.
+                  Recebeu: #{hash_error[:value]}
+      TEXT
+    }
+  end
 end
