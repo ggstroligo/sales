97eb0661e29ad2ccf30e7fa85ca2268679a48555
ggstroligo <gutostroligo@gmail.com>
2021-07-03 01:57:51 -0300

    ( + ) helper methods

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -8,11 +8,32 @@ module SalesHelper
 
   def error_msg(hash_error)
     {
-      title: "Linha 1, Coluna #{hash_error[:index]}",
+      title: hash_error[:type],
+      info: "Linha #{(hash_error[:line] || 0) + 1}, Coluna #{(hash_error[:column] || 0) + 1}",
       description: <<-TEXT
                   Esperava: #{hash_error[:expected]}.
                   Recebeu: #{hash_error[:value]}
       TEXT
     }
+  end
+
+  def readable?(file)
+    file_type, status = Open3.capture2e('file', file.path)
+    status.success? && file_type.include?('text')
+  end
+
+  def report_file
+    sales_params.open
+  end
+
+  def extracted_data
+    @extracted_data ||= report_file.read
+  end
+
+  def check_file
+    return if readable? report_file
+
+    flash[:error] = [error_msg(type: 'Invalid file type', expected: 'Arquivo de texto', value: 'Binário')]
+    redirect_to new_sale_path
   end
 end
